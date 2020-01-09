a350af7b3a4ba830af6485cfbf0079bf900f1e25
Bystroushaak <bystrousak@kitakitsune.org>
2020-01-09 15:17:35 +0100

    Added classify.py.

---
+++ classify.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+import sys
+import json
+import os.path
+import argparse
+from markov import compare_models
+from markov import train_char_model
+
+
+def read_models(models_root):
+    for fn in os.listdir(models_root):
+        path = os.path.join(models_root, fn)
+        if not os.path.isfile(path) or not fn.endswith(".json"):
+            continue
+
+        with open(path) as f:
+            model = json.load(f)
+
+        model_name = str(fn).rsplit(".", 1)[0]
+
+        yield model_name, model
+
+
+def classify(what, models_root, print_details=True):
+    classified_model = train_char_model(what, 4)
+
+    results = []
+    for model_name, model in read_models(models_root):
+        result = compare_models(classified_model, model)
+
+        if print_details:
+            print(model_name, result)
+
+        results.append([result, model_name])
+
+    return max(results)[1]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Print details."
+    )
+    parser.add_argument(
+        "filename"
+    )
+    args = parser.parse_args()
+
+    if args.filename == "-":
+        what = sys.stdin.read()
+    elif os.path.isfile(args.filename):
+        with open(args.filename) as f:
+            what = f.read()
+    else:
+        sys.stderr.write("`%s` not found.\n" % args.filename)
+        sys.exit(1)
+
+    print(classify(what, "models", args.verbose))
